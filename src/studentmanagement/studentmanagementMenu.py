da62b18b808715ffe1398a7c792c19b2a3d036b9
Doziechucks <chiedoziechukwukam@gmail.com>
2025-03-14 14:57:43 +0100

    main update

--- src/studentmanagement/studentmanagementMenu.py
+++ src/studentmanagement/studentmanagementMenu.py
@@ -7,7 +7,7 @@ from src.studentmanagement.exception import passwordException, passwordValidateE
 
 users = UserManagement()
 
-def mainMenu():
+def main_menu():
     while True:
         menu = input("""
    !!!! Welcome to ADC Student Management System !!!!
@@ -27,7 +27,7 @@ def mainMenu():
                 case "4":
                     student_login()
                 case "0":
-                    mainMenu()
+                    main_menu()
                 case _:
                     print("Enter correct input!!!")
 
@@ -42,13 +42,19 @@ def student_register():
     register(check)
 
 
+
 def student_login():
     checker = 2
     login_in(checker)
 
 def facilitator_login():
     checker = 1
-    login_in(checker)
+    logger = login_in(checker)
+    if logger != "":
+        facilitator_choice(logger)
+
+
+
 
 
 def login_in(checker):
@@ -56,11 +62,14 @@ def login_in(checker):
     password = input("Please enter your password: ")
     if checker == 1:
         users.facilitator_loging(email, password)
+        print(users.check_facilitator_logged_in(email))
         while not users.check_facilitator_logged_in(email):
             print("Invalid login attempt")
             email = input("Please enter your facilitator email: ")
             password = input("Please enter your facilitator password: ")
             users.facilitator_loging(email, password)
+        else:
+            print("Login successful")
     if checker == 2:
         users.student_login(email, password)
         while not users.check_student_logged_in(email):
@@ -68,47 +77,64 @@ def login_in(checker):
             email = input("Please enter your email: ")
             password = input("Please enter your password: ")
             users.facilitator_loging(email, password)
+        else:
+            print("Login successful")
+    return email
 
 def register(check):
-    firstname = input("Please enter your first name: ")
-    try:
-        if not re.search("[A-Za-z]", firstname):
-            raise nameException
-    except nameValidationException as e:
-        print(f"Invalid first name: {e}")
+    done = False
+    firstname = ""
+    while not done:
+        try:
+            firstname = input("Please enter your first name: ")
+            if not re.search("[A-Za-z]", firstname):
+                raise nameException
+            else: done = True
+        except nameValidationException as e:
+            print(f"Invalid first name: {e}")
 
-    lastname = input("Please enter your last name: ")
-    try:
-        if not re.search("[A-Za-z]", lastname):
-            raise nameException
-    except nameValidationException as e:
-        print(f"Invalid last name: {e}")
+    move = False
+    lastname = ""
+    while not move:
+        try:
+            lastname = input("Please enter your last name: ")
+            if not re.search("[A-Za-z]", lastname):
+                raise nameException
+            else: move = True
+        except nameValidationException as e:
+            print(f"Invalid last name: {e}")
 
-    facilitator_password = input("Please enter your password: ")
-    try:
-        if not re.search("[A-Za-z0-9]", facilitator_password) or len(facilitator_password) < 4:
-            raise passwordException
-        else:
-            print("Your password saved successfully.")
-    except passwordValidateException as e:
-        print(f"invalid password: {e}")
+    facilitator_password = ""
+    pass_check = False
+    while not pass_check:
 
-    facilitator_email = input("Please enter your email: ")
-    try:
-        validateEmail = validate_email(facilitator_email)
-        print(f"Valid emailAddress: {validateEmail} saved successfully")
-        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-        if not re.match(pattern, facilitator_email):
-            raise emailException(facilitator_email)
-        return facilitator_email
-    except emailException as e:
-        print(f"Invalid email: {e}")
+        try:
+            facilitator_password = input("Please enter your password: ")
+            if not re.search("[A-Za-z0-9]", facilitator_password) or len(facilitator_password) < 4:
+                raise passwordException
+            else: pass_check = True
+        except passwordValidateException as e:
+            print(f"invalid password: {e}")
+
+    facilitator_email = ""
+    mail_check = False
+    while not mail_check:
+        try:
+            facilitator_email = input("Please enter your email: ")
+            validate_email(facilitator_email)
+            pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+            if not re.match(pattern, facilitator_email):
+                raise emailException(facilitator_email)
+            else: mail_check = True
+        except emailException as e:
+            print(f"Invalid email: {e}")
+
     if check == 1:
         users.create_facilitator_list(firstname, lastname, facilitator_email, facilitator_password)
+        print("Facilitator list created")
     elif check == 2:
         users.create_student_list(firstname, lastname, facilitator_email, facilitator_password)
 
-<<<<<<< HEAD
 def facilitator_choice(email):
     option = input("""
     1. create course
@@ -148,9 +174,10 @@ def grade_student(email):
     grade = input("Please enter grade for student: ")
     users.grade_a_particular_course(course_id, student_email, grade, email)
     facilitator_choice(email)
-=======
->>>>>>> refs/remotes/origin/main
 
 
 
-mainMenu()
+
+
+
+main_menu()
